0120ad153ed6b6390f58db2bb87ca321a0b10670
Jordi Weijers <s2228483@frodo.iblbioinfo.lu.nl>
2025-08-12 14:22:46 +0200

    Changed making of presence absence tables to use NumPy for increased performance

--- bin/main.py
+++ bin/main.py
@@ -2,7 +2,17 @@ import argparse
 import os
 import multiprocessing
 import pandas as pd
-from bin.utils import genbank_to_protein_fasta, make_diamond_db, run_diamond, make_presence_absence_table, calculate_jaccard_similarity_scores, construct_matrix, find_medoid, calculate_representation_scores
+from bin.utils import (
+	genbank_to_protein_fasta,
+	make_diamond_db,
+	run_diamond,
+	make_presence_absence_table,
+	write_pa_table_to_csv,
+	calculate_jaccard_similarity_scores,
+	construct_matrix,
+	find_medoid,
+	calculate_representation_scores
+)
 
 description = """
 Find the most representative genome from a set of similar genomes, based on the presence/absence of genes.
@@ -10,65 +20,97 @@ Find the most representative genome from a set of similar genomes, based on the 
 Developer: Jordi Weijers
 Affiliation: Institute of Biology Leiden, Leiden University
 Please contact Jordi at s2228483@vuw.leidenuniv.nl if you have any issues.
-Version: v0.2.0"""
+Version: v0.3.0
+"""
 
 def parse_arguments():
 	""" Setup argument parsing and return parsed arguments."""
 	parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
 	input_group = parser.add_argument_group("Input Options")
-	input_group.add_argument('-i', '--input_dir', type=str, required=True, help="Path to the input directory containing GenBank files.", metavar="")
+	input_group.add_argument(
+		'-i', '--input_dir',
+		type=str, required=True,
+		help="Path to the input directory containing GenBank files.",
+		metavar=""
+	)
 	output_group = parser.add_argument_group("Output Options")
-	output_group.add_argument('-o', '--output_dir', type=str, required=True, help="Path to save the output directory to.", metavar="")
-	parser.add_argument('-qc', '--query_cover', type=float, default=70.0, help="Minimum percent query cover for diamond search. (Default: 70)", metavar="")
-	parser.add_argument('-id', '--identity', type=float, default=35.0, help="Minimum percent identity for diamond search. (Default: 35)", metavar="")
+	output_group.add_argument(
+		'-o', '--output_dir',
+		type=str, required=True,
+		help="Path to save the output directory to.",
+		metavar=""
+	)
+	parser.add_argument(
+		'-qc', '--query_cover',
+		type=float,
+		default=70.0,
+		help="Minimum percent query cover for diamond search. (Default: 70)",
+		metavar=""
+	)
+	parser.add_argument(
+		'-id', '--identity',
+		type=float,
+		default=35.0,
+		help="Minimum percent identity for diamond search. (Default: 35)",
+		metavar=""
+	)
+	parser.add_argument(
+		'-c', '--cpus',
+		type=int,
+		default=multiprocessing.cpu_count(),
+		help=f"Number of CPU cores to use for parallel processing. Use 0 for all available cores (Default: {multiprocessing.cpu_count()})",
+		metavar=""
+	)
 	return parser.parse_args()
 
-def validate_arguments(genbank_dir: str, output_dir: str, query_cover: float, identity: float) -> None:
-	""" Validate the arguments."""
+def validate_arguments(genbank_dir: str, output_dir: str, query_cover: float, identity: float, cpus: int) -> int:
+	""" Validate the arguments and return the number of CPUs to use."""
 	if not os.path.isdir(genbank_dir):
-	        raise ValueError(f"Error: The input directory '{genbank_dir}' does not exists. Please provide a directory containing GenBank files.")
+		raise ValueError(f"Error: The input directory '{genbank_dir}' does not exist. Please provide a directory containing GenBank files.")
 	for filename in os.listdir(genbank_dir):
-        	if not filename.endswith(".gbff"):
-                	raise ValueError(f"Error: The input directory '{genbank_dir}' contains non-GenBank files. Please provide a directory ONLY containing GenBank files.")
+		if not filename.endswith(".gbff"):
+			raise ValueError(f"Error: The input directory '{genbank_dir}' contains non-GenBank files. Please provide a directory ONLY containing GenBank files.")
 	if os.path.exists(output_dir):
-        	raise ValueError(f"Error: The output directory '{output_dir}' already exists. Please provide a new directory.")
+		raise ValueError(f"Error: The output directory '{output_dir}' already exists. Please provide a new directory.")
 	else:
-        	os.makedirs(output_dir)
+		os.makedirs(output_dir)
 	if not (0 <= query_cover <= 100):
 		raise ValueError("Error: --query_cover must be between 0 and 100.")
+	if cpus < 0 or cpus > multiprocessing.cpu_count():
+		raise ValueError(f"Error: --cpus must be between 0 and {multiprocessing.cpu_count()}.")
+	if cpus == 0:
+		cpus = multiprocessing.cpu_count()
+	return cpus
 
 def process_genbank(filename, genbank_dir, pa_dir, blast_output_tsv, pa_tables):
 	"""
-	Process a GenBank file and make a presence absence table and calculate the representation score
+	Process a GenBank file and make presence absence table
 	"""
-	# Making the presence absence table
 	basename, ext = os.path.splitext(filename)
 	pa_table_path = os.path.join(pa_dir, f"{basename}.csv")
-	pa_table = make_presence_absence_table(filename, blast_output_tsv)
-	pa_table.to_csv(pa_table_path)
-	pa_tables[basename] = pa_table
+	pa_table, genome_map_index, gene_map_index = make_presence_absence_table(filename, blast_output_tsv)
+	write_pa_table_to_csv(pa_table, genome_map_index, gene_map_index, pa_table_path)
+	pa_tables[basename] = (pa_table, genome_map_index, gene_map_index)
 
-def main ():
-	# Parse and Validate arguments
+def main():
+	# Parse and validate arguments
 	args = parse_arguments()
 	genbank_dir = args.input_dir
 	output_dir = args.output_dir
 	query_cover = args.query_cover
 	identity = args.identity
-	validate_arguments(genbank_dir, output_dir, query_cover, identity)
-
-	# Initialize variables
-	pa_tables = {}
+	cpus = validate_arguments(genbank_dir, output_dir, query_cover, identity, args.cpus)
 
 	# Make protein FASTA files
 	print("Making protein FASTA files")
+	protein_fasta_dir = os.path.join(output_dir, "protein_fasta")
+	os.makedirs(protein_fasta_dir, exist_ok=True)
+	log_file = os.path.join(protein_fasta_dir, "fasta.log")
 	for filename in os.listdir(genbank_dir):
 		genbank_file = os.path.join(genbank_dir, filename)
 		basename, ext = os.path.splitext(filename)
-		protein_fasta_dir = os.path.join(output_dir, "protein_fasta")
-		os.makedirs(protein_fasta_dir, exist_ok=True)
 		protein_fasta = os.path.join(protein_fasta_dir, f"{basename}.fna")
-		genbank_to_protein_fasta(genbank_file, protein_fasta)
+		genbank_to_protein_fasta(genbank_file, protein_fasta, log_file)
 
 	# Make a combined protein FASTA file
 	all_protein_fasta = os.path.join(output_dir, "all_protein.fna")
@@ -78,24 +120,25 @@ def main ():
 				fasta_path = os.path.join(protein_fasta_dir, fasta_file)
 				with open(fasta_path, "r") as infile:
 					outfile.write(infile.read())
+
 	# Make a DIAMOND database
 	print("Making the DIAMOND database")
 	diamond_db = os.path.join(output_dir, "all_protein.dmnd")
-	make_diamond_db(all_protein_fasta, diamond_db)
+	make_diamond_db(all_protein_fasta, diamond_db, cpus)
 
 	# Run DIAMOND all vs all
 	print("Running the DIAMOND all vs all")
 	blast_output_tsv = os.path.join(output_dir, "blast.tsv")
-	run_diamond(all_protein_fasta, diamond_db, blast_output_tsv, query_cover, identity)
+	run_diamond(all_protein_fasta, diamond_db, blast_output_tsv, query_cover, identity, cpus)
 
-	# Make a presence absence table for each GenBank file based on the DIAMOND output
+	# Make presence-absence tables
 	print("Making the presence-absence tables")
 	pa_dir = os.path.join(output_dir, "presence_absence")
 	os.makedirs(pa_dir, exist_ok=True)
-	# Use multiprocessing for parallel processing
+
 	manager = multiprocessing.Manager()
 	pa_tables = manager.dict()
-	pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+	pool = multiprocessing.Pool(processes=cpus)
 	tasks = [(filename, genbank_dir, pa_dir, blast_output_tsv, pa_tables) for filename in os.listdir(genbank_dir)]
 	pool.starmap(process_genbank, tasks)
 	pool.close()
@@ -105,19 +148,21 @@ def main ():
 	# Calculating the Jaccard similarity scores
 	print("Calculating the Jaccard similarity scores")
 	jaccard_similarity_scores = calculate_jaccard_similarity_scores(pa_tables)
+
 	# Write the Jaccard similarity scores to matrix files
 	jaccard_matrix_dir = os.path.join(output_dir, "jaccard_similarity_matrices")
 	os.makedirs(jaccard_matrix_dir, exist_ok=True)
 	for ref_genbank_filename, jac_sim_scores in jaccard_similarity_scores.items():
-		jac_sim_df = pd.DataFrame(list(jac_sim_scores.items()), columns=["Other_GenBank", "Jaccad_Similarity_Score"])
+		jac_sim_df = pd.DataFrame(list(jac_sim_scores.items()), columns=["Other_GenBank", "Jaccard_Similarity_Score"])
 		jac_sim_df.insert(0, "Reference_GenBank", ref_genbank_filename)
 		output_file = os.path.join(jaccard_matrix_dir, f"{ref_genbank_filename}_jaccard_matrix.csv")
 		jac_sim_df.to_csv(output_file, index=False)
+
 	# Concatenate the Jaccard matrices
 	jaccard_matrix = construct_matrix(jaccard_similarity_scores)
-	# Write the concatenated Jaccard matrix to file
 	jaccard_matrix_path = os.path.join(output_dir, "jaccard_matrix.csv")
 	jaccard_matrix.to_csv(jaccard_matrix_path)
+
 	# Calculate the representation scores
 	representation_scores_df = calculate_representation_scores(jaccard_matrix)
 	output_file = os.path.join(output_dir, "representation_scores.tsv")
--- bin/utils.py
+++ bin/utils.py
@@ -2,37 +2,65 @@ import os
 from Bio import SeqIO
 import shutil
 import subprocess
+import csv
 import pandas as pd
 import numpy as np
 from sklearn.manifold import MDS
 
-def genbank_to_protein_fasta(genbank_file: str, protein_fasta: str) -> None:
+def genbank_to_protein_fasta(genbank_file: str, protein_fasta: str, log_file: str) -> None:
 	"""
 	Converts a GenBank file to a FASTA file containing protein coding sequences.
+	Skips CDS features without translations (e.g., pseudogenes or partial sequences).
+	Logs summary statistics to a specified log file.
 
 	Args:
 		genbank_file: Path to input GenBank file.
 		protein_fasta: Path to output protein FASTA file.
-	Returns:
-		None
+		log_file: Path to log file to append processing statistics.
 	"""
 	lines = []
-	basename =  os.path.splitext(os.path.basename(genbank_file))[0]
+	basename = os.path.splitext(os.path.basename(genbank_file))[0]
+	written_count = 0
+	skipped_no_translation = 0
+	skipped_pseudogene = 0
+
 	for record in SeqIO.parse(genbank_file, "genbank"):
 		for feature in record.features:
-			if feature.type == "CDS":
-				locus_tag = feature.qualifiers.get("locus_tag",  ["N/A"])[0].split("|")[0]
-				location = str(feature.location)
-				location = location.replace(" ", "")
-				lines.append(">" + locus_tag + "|" + location + "|" + basename)
-				protein_seq = str(feature.qualifiers.get("translation")[0])
-				lines.append(protein_seq)
-	with open(protein_fasta, "a") as file:
-		for line in lines:
-			file.write(line + '\n')
-		file.close()
+			if feature.type != "CDS":
+				continue
 
-def make_diamond_db(protein_fasta: str, dmnd_db: str, dmnd_path: str = "diamond") -> None:
+			# Skip pseudogenes
+			if "pseudo" in feature.qualifiers or "pseudogene" in feature.qualifiers:
+				skipped_pseudogene += 1
+				continue
+
+			# Get translation
+			translation = feature.qualifiers.get("translation")
+			if not translation:
+				skipped_no_translation += 1
+				continue
+
+			locus_tag = feature.qualifiers.get("locus_tag", ["N/A"])[0].split("|")[0]
+			location = str(feature.location).replace(" ", "")
+			protein_seq = str(translation[0]).replace(" ", "").replace("\n", "")
+
+			lines.append(f">{locus_tag}|{location}|{basename}")
+			lines.append(protein_seq)
+			written_count += 1
+
+	# Append sequences to output FASTA
+	with open(protein_fasta, "a") as file:
+		file.write("\n".join(lines) + "\n")
+
+	# Append log entry
+	with open(log_file, "a") as log:
+		log.write(
+			f"{basename}: Wrote {written_count} protein sequences | "
+			f"Skipped {skipped_no_translation} with no translation | "
+			f"Skipped {skipped_pseudogene} pseudogenes\n"
+		)
+
+def make_diamond_db(protein_fasta: str, dmnd_db: str, cpus: int, dmnd_path: str = "diamond") -> None:
 	"""
 	Makes a DIAMOND databases from a FASTA file containing protein sequences.
 
@@ -50,11 +78,12 @@ def make_diamond_db(protein_fasta: str, dmnd_db: str, dmnd_path: str = "diamond"
 		dmnd_path, 'makedb',
 		"--in", protein_fasta,
 		"--db", dmnd_db,
+		"--threads", str(cpus),
 		"--quiet"
 	]
 	subprocess.run(command, check=True)
 
-def run_diamond(query: str, dmnd_db: str, tsv_file: str, query_cover: float, identity: float, dmnd_path: str = "diamond") -> None:
+def run_diamond(query: str, dmnd_db: str, tsv_file: str, query_cover: float, identity: float, cpus: int, dmnd_path: str = "diamond") -> None:
 	"""
 	Runs DAIMOND BLAST to align protein sequences against a DIAMOND database.
 
@@ -64,6 +93,7 @@ def run_diamond(query: str, dmnd_db: str, tsv_file: str, query_cover: float, ide
 		tsv_file: Path to tsv file containing the output.
 		query_cover: A float representing the minimum query cover percentage.
 		identity: A float representing the minimum identity percentage.
+		cpues: The Number of CPUs to use.
 		dmnd_path: Path to the DIAMOND executable (default is 'diamond'if it is in the PATH).
 	Returns:
 		None
@@ -78,72 +108,105 @@ def run_diamond(query: str, dmnd_db: str, tsv_file: str, query_cover: float, ide
 		"--out", tsv_file,
 		"--query-cover", str(query_cover),
 		"--id", str(identity),
+		"--threads", str(cpus),
 		"-k0",
 		"--quiet",
 		"--outfmt", "6", "qseqid", "sseqid"
 	]
 	subprocess.run(command, check=True)
 
-def make_presence_absence_table(genbank_filename: str, blast_output: str) -> pd.DataFrame:
+def make_presence_absence_table(genbank_filename: str, blast_output: str) -> tuple[np.ndarray, dict, dict]:
 	"""
 	Generate a presence absence table for a given GenBank filename based on the DIAMOND blast output.
 	Args:
 		genbank_filename: The name of a GenBank file.
 		blast_output: Path to the DIAMOND BLAST output file.
 	Returns:
-		presence_absence: A Pandas DataFrame representing the presence absence table.
+		presence_absence: NumPy array of shape (num_s_genomes, num_q_genes)
+		genome_map_index: A Dictionary mapping the genome name to the row index
+		gene_map_index: A Dictionary mapping the gene name to the column index
 	"""
 	basename = os.path.splitext(genbank_filename)[0]
-	columns = ["qseqid", "sseqid"]
-	blast_df = pd.read_csv(blast_output, sep="\t", header=None, names=columns)
-	blast_df = blast_df[blast_df["qseqid"].str.endswith(basename)]
-	current_genes = blast_df["qseqid"].unique()
-	other_genbank_names = blast_df["sseqid"].apply(lambda x: x.split("|")[-1]).unique()
-	presence_absence = pd.DataFrame(0, index=other_genbank_names, columns=current_genes)
-	for name in other_genbank_names:
-		for qseqid in current_genes:
-			hits = blast_df[
-				(blast_df["qseqid"] == qseqid) &
-				(blast_df["sseqid"].str.split("|").str[-1] == name)
-			]
-			presence_absence.at[name, qseqid] = len(hits)
-	return presence_absence
+	genes = []
+	genomes = []
+	with open(blast_output, newline = '',) as file:
+		reader = csv.reader(file, delimiter = "\t")
+		for row in reader:
+			qseqid, sseqid = row[0], row[1]
+			if qseqid.endswith(basename):
+				genes.append(qseqid)
+				genomes.append(sseqid.split("|")[-1])   # extract only the genome name
 
-def calculate_jaccard_similarity_scores(pa_tables: dict[str, pd.DataFrame]) -> dict[str, dict[str, float]]:
+	if not genes:
+		return np.zeros((0,0), dtype=int), [], []
+
+	genes = np.array(genes)
+	genomes = np.array(genomes)
+
+	unique_genes, genes_indices = np.unique(genes, return_inverse=True)
+	unique_genomes, genomes_indices = np.unique(genomes, return_inverse=True)
+
+	presence_absence = np.zeros((len(unique_genomes), len(unique_genes)), dtype=int)
+	np.add.at(presence_absence, (genomes_indices, genes_indices), 1)
+
+	genome_index_map = {genome: i for i, genome in enumerate(unique_genomes)}
+	gene_index_map = {gene: j for j, gene in enumerate(unique_genes)}
+	return presence_absence, genome_index_map, gene_index_map
+
+def write_pa_table_to_csv(pa_matrix: np.ndarray, genome_map_index: dict[str, int], gene_map_index: dict[str, int], output_path_csv: str) -> None:
 	"""
-	Calculate the Jaccard similarity scores based on a dictionary containing all the presence-absence tables.
+	Write the presence absence matrix to a CSV file with in the genomes as rows and the genes as columns
 	Args:
-		pa_tables: A Dictionary containing all the presence-absence tables.
+		pa_matrix: NumPy array of shape (num_s_genomes, num_q_genes)
+		genome_map_index: A Dictionary mapping the genome name to a row index
+		gene_map_index: A Dictionary mapping the gene name to a column index
+	output_path_csv: The Path where to save the CSV file
 	Returns:
-		jaccard_similarity_scores: A Dictionary containing the different Jaccard similarity matrices for each presence-absence table.
+		None
+	"""
+	genomes_sorted = [genome for genome, idx in sorted(genome_map_index.items(), key=lambda x: x[1])]
+	genes_sorted = [gene for gene, idx in sorted(gene_map_index.items(), key=lambda x: x[1])]
+	with open(output_path_csv, mode="w", newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(["Gene Names"] + genes_sorted)
+		for genome in genomes_sorted:
+			row_idx = genome_map_index[genome]
+			row_data = pa_matrix[row_idx, :]
+			writer.writerow([genome] + row_data.tolist())
+
+def calculate_jaccard_similarity_scores(pa_tables: dict[str, dict[np.array, dict, dict]]) -> dict[str, dict[str, float]]:
+	"""
+	Calculate the Jaccard similarity scores based on a dictioynary containing all the presence-absence tables.
+	Args:
+		pa_tables: A Dictionary containing all the presence-absence matrices and the corresponding genome and gene mapping dictionaries.
+	Returns:
+	jaccard_similarity_scores: A Dictionary containing the different Jaccard similarity matrices for each presence-absence table.
 	"""
 	jaccard_similarity_scores = {}
-	for ref_genbank_filename, pa_table in pa_tables.items():
-		if ref_genbank_filename not in pa_table.index:
-			raise ValueError(f"Reference genome '{ref_genbank_filename}' not found in the pa_table index '{pa_table}'.")
-		gene_frequency = {}
-		for gene_name in pa_table.columns:
-			gene_freq = (pa_table[gene_name] > 0).sum()
-			gene_frequency[gene_name] = gene_freq
-		max_frequency = max(gene_frequency.values())
-		weights = {}
-		for gene_name, freq in gene_frequency.items():
-			weights[gene_name] = 1 - (freq / max_frequency)
-		ref_vector = pa_table.loc[ref_genbank_filename]
-		jac_sim_scores = {}
-		for other_genbank_filename in pa_table.index:
-			other_vector = pa_table.loc[other_genbank_filename]
-			weighted_intersection = 0
-			weighted_union = 0
-			for gene_name in pa_table.columns:
-				weight = weights[gene_name]
-				count_ref = ref_vector[gene_name]
-				count_other = other_vector[gene_name]
-				weighted_intersection += weight * min(count_ref, count_other)
-				weighted_union += weight * max(count_ref, count_other)
-			jac_sim = weighted_intersection / weighted_union if weighted_union != 0 else 0.0
-			jac_sim_scores[other_genbank_filename] = jac_sim
-		jaccard_similarity_scores[ref_genbank_filename] = jac_sim_scores
+	for ref_genbank_filename, (pa_matrix, genome_map_index, gene_map_index) in pa_tables.items():
+		if ref_genbank_filename not in genome_map_index:
+			raise ValueError(f"Reference genome '{ref_genbank_filename}' not found in row labels {genome_map_index}")
+
+		gene_freq = (pa_matrix > 0).sum(axis=0)
+		max_freq = gene_freq.max()
+		if max_freq == 0:
+			weights = np.zeros_like(gene_freq, dtype=float)
+		else:
+			weights = 1 - (gene_freq / max_freq)
+
+		ref_idx = genome_map_index[ref_genbank_filename]
+		ref_vector = pa_matrix[ref_idx, :]
+		min_matrix = np.minimum(pa_matrix, ref_vector)
+		max_matrix = np.maximum(pa_matrix, ref_vector)
+
+		weighted_intersection = np.sum(weights * min_matrix, axis=1)
+		weighted_union = np.sum(weights * max_matrix, axis=1)
+
+		with np.errstate(divide="ignore", invalid="ignore"):
+			jaccard_scores = np.where(weighted_union != 0, weighted_intersection / weighted_union, 0.0)
+
+		jac_dict = {genome_name: float(jaccard_scores[idx]) for genome_name, idx in genome_map_index.items()}
+		jaccard_similarity_scores[ref_genbank_filename] = jac_dict
 	return jaccard_similarity_scores
 
 def construct_matrix(jaccard_similarity_scores: dict[str, dict[str, float]]) -> pd.DataFrame:
