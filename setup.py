62997b9339cd59f1bd2a2395193ed9685f853d99
jordiweijers <jordiw1999.jw@gmail.com>
2025-10-06 22:36:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	name="find_representative",
 	version=VERSION,
 	author="Jordi Weijers",
-	author_email="s2228483@vuw.leidenuniv.nl",
+	author_email="jweijers@ifam.uni-kiel.de",
 	description=DESCRIPTION,
 	packages=find_packages(),
 	entry_points={'console_scripts': ["find_representative=bin.main:main"]}
